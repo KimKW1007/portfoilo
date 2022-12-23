df357426ac18ba1dd4b81d58108edd2e21bf63ca
KimKW1007 <face676934@gmail.com>
2022-12-23 21:42:14 +0900

    chore : api 수정 및 prisma 삭제

--- src/api/clientApi.js
+++ src/api/clientApi.js
@@ -1,22 +1,23 @@
 import axios from 'axios';
-
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
 const baseApi =  axios.create({
+  baseURL: publicRuntimeConfig.backendUrl,
   headers: {
     "Content-Type": "application/json",
   },
 })
-
 export const createComment = async(data) =>{
   try{
-    const res = await baseApi.post("http://localhost:5001/comments", data)
+    const res = await baseApi.post("comments", data)
   }catch(err){
     console.log(err);
   }
 }
 export const getComment = async() =>{
   try{
-    const res = await baseApi.get("http://localhost:5001/comments")
+    const res = await baseApi.get("comments")
     return res.data;
   }catch(err){
     console.log(err);
@@ -25,7 +26,7 @@ export const getComment = async() =>{
 export const deleteComment = async({commentId, password}) =>{
   try{
     console.log({commentId})
-    const res = await baseApi.delete("http://localhost:5001/comments", {
+    const res = await baseApi.delete("comments", {
       data : {commentId, password}
     })
   }catch(err){
@@ -34,7 +35,7 @@ export const deleteComment = async({commentId, password}) =>{
 }
 export const getProjectList = async() =>{
   try{
-    const res = await baseApi.get("http://localhost:5001/projects")
+    const res = await baseApi.get("projects")
     return res.data;
   }catch(err){
     console.log(err);
