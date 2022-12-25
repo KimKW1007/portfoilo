1a0bf9d4460990d59905bf061e59e8d73c5e9b43
KimKW1007 <face676934@gmail.com>
2022-12-25 19:51:08 +0900

    chore : axios baseURL 변경

--- src/api/clientApi.js
+++ src/api/clientApi.js
@@ -2,21 +2,25 @@ import axios from 'axios';
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
+
+
 const baseApi =  axios.create({
   headers: {
     "Content-Type": "application/json",
   },
 })
+
+baseApi.defaults.baseURL = 'https://port-0-portfoilo-back-ll32glc0g30q9.gksl2.cloudtype.app';
 export const createComment = async(data) =>{
   try{
-    const res = await baseApi.post(`https://port-0-portfoilo-back-ll32glc0g30q9.gksl2.cloudtype.app/comments`, data)
+    const res = await baseApi.post(`/comments`, data)
   }catch(err){
     console.log(err);
   }
 }
 export const getComment = async() =>{
   try{
-    const res = await baseApi.get(`https://port-0-portfoilo-back-ll32glc0g30q9.gksl2.cloudtype.app/comments`)
+    const res = await baseApi.get(`/comments`)
     return res.data;
   }catch(err){
     console.log(err);
@@ -25,7 +29,7 @@ export const getComment = async() =>{
 export const deleteComment = async({commentId, password}) =>{
   try{
     console.log({commentId})
-    const res = await baseApi.delete(`https://port-0-portfoilo-back-ll32glc0g30q9.gksl2.cloudtype.app/comments`, {
+    const res = await baseApi.delete(`/comments`, {
       data : {commentId, password}
     })
   }catch(err){
@@ -34,7 +38,7 @@ export const deleteComment = async({commentId, password}) =>{
 }
 export const getProjectList = async() =>{
   try{
-    const res = await baseApi.get(`https://port-0-portfoilo-back-ll32glc0g30q9.gksl2.cloudtype.app/projects`)
+    const res = await baseApi.get(`/projects`)
     return res.data;
   }catch(err){
     console.log(err);
