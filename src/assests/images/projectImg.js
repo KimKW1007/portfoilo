b2cb6d35afc62f2d20b04387dc4b72948f41dfe4
KimKW1007 <face676934@gmail.com>
2022-12-26 16:10:02 +0900

    chore : 이미지 추가

--- src/assests/images/projectImg.js
+++ src/assests/images/projectImg.js
@@ -1,20 +1,19 @@
+import bodytory from './bodytory.png';
+import dasida from './dasida.png';
 import wepo from './wepo.png';
 import megabox from './megabox.png';
 import naverClova from './naverClova.png';
 import daelim from './daelim.png';
-import pofo from './pofo.png';
 import litho from './litho.png';
-import tomntoms from './tomntoms.png';
-import starbucks from './starbucks.png';
+
 
 
 export {
+  bodytory,
+  dasida,
   wepo,
   megabox,
   naverClova,
   litho,
   daelim,
-  starbucks,
-  pofo,
-  tomntoms,
 }
